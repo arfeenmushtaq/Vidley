1156bbef028cb83c987fc2cac6d9683c67397464
info soft <info soft@INFOSOFT-PC>
2017-10-01 14:18:56 +0500

    Add project files.

---
+++ Vidley/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vidley.Startup))]
+namespace Vidley
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
